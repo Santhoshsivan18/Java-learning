eec160e5b4a55725e6e4da5a9834d09ba6130ccc
Santhoshsivan18 <2002santhoshsivan@gmail.com>
2024-06-12 07:52:59 +0530

    yes

--- sortingmethods.java
+++ sortingmethods.java
@@ -1,28 +1,152 @@
-import java.util.*;
-
-public class sortingmethods {
-    public static void main(String[] args) {
-        int[] a = {8, 2, 9, 3, 1, 6, 4};
-        // array length is 7
-
-        System.out.println("Original Array: ");
-        System.out.println(Arrays.toString(a));
-
-        //bubble sort
-        int[] bubble = Arrays.copyOf(a, a.length);
-        for(int i=0; i<bubble.length; i++){
-            for(int j=0; j<bubble.length-i-1; j++){
-                if(bubble[j] > bubble[j+1]){
-                    int t = bubble[j];
-                    bubble[j] = bubble[j+1];
-                    bubble[j+1] = t;
+class Sorting {
+    public static void SelectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min_value = arr[i];
+            int min_index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min_value > arr[j]) {
+                    min_value = arr[j];
+                    min_index = j;
                 }
-                System.out.println(Arrays.toString(bubble));
+            }
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+    }
+
+    public static void BubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
-        System.out.println("Bubble Sort: ");
-        System.out.println(Arrays.toString(bubble));
+    }
 
+    public static void InsertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
 
+    public static void display(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void merge(int a[], int beg, int mid, int end) {
+        int i, j, k;
+        int n1 = mid - beg + 1;
+        int n2 = end - mid;
+
+        /* temporary Arrays */
+        int LeftArray[] = new int[n1];
+        int RightArray[] = new int[n2];
+
+        /* copy data to temp arrays */
+        for (i = 0; i < n1; i++)
+            LeftArray[i] = a[beg + i];
+        for (j = 0; j < n2; j++)
+            RightArray[j] = a[mid + 1 + j];
+
+        i = 0; /* initial index of first sub-array */
+        j = 0; /* initial index of second sub-array */
+        k = beg; /* initial index of merged sub-array */
+
+        while (i < n1 && j < n2) {
+            if (LeftArray[i] <= RightArray[j]) {
+                a[k] = LeftArray[i];
+                i++;
+            } else {
+                a[k] = RightArray[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            a[k] = LeftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            a[k] = RightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int a[], int beg, int end) {
+        if (beg < end) {
+            int mid = (beg + end) / 2;
+            mergeSort(a, beg, mid);
+            mergeSort(a, mid + 1, end);
+            merge(a, beg, mid, end);
+        }
+    }
+
+    /* function to implement quick sort */
+    public static void quickSort(int arr[], int lowIndex, int highIndex) /*
+                                                                          * a[] = array to be sorted, start = Starting
+                                                                          * index, end = Ending index
+                                                                          */
+    {
+        if (lowIndex >= highIndex) {
+            return;
+        }
+        int pivot = arr[highIndex];
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        while (leftPointer < rightPointer) {
+            while (arr[leftPointer] <= pivot && leftPointer < rightPointer) {
+                leftPointer++;
+            }
+
+            while (arr[rightPointer] >= pivot && leftPointer < rightPointer) {
+                rightPointer--;
+            }
+
+            swap(arr, leftPointer, rightPointer);
+        }
+        swap(arr, leftPointer, highIndex);
+
+        quickSort(arr, lowIndex, leftPointer - 1);
+        quickSort(arr, leftPointer + 1, highIndex);
+    }
+
+    public static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 7, 3, 5, 1 };
+        quickSort(arr, 0, arr.length - 1);
+        display(arr);
     }
 }
